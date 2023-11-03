ae7fb30da9b9aeb8779fabe9f686cab9cbba4e1e
Ravel Mesquita <ravels78@gmail.com>
2023-11-03 15:34:10 -0300

    Inicio do Projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using GerenciadorDeEstoque.Telas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorDeEstoque
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TelaLogin());
+        }
+    }
+}
---
+++ Telas/TelaLogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GerenciadorDeEstoque.Properties;
+
+
+
+namespace GerenciadorDeEstoque.Telas
+{
+
+    public partial class TelaLogin : Form
+    {
+        LoginAcesso loginAcesso = new LoginAcesso();
+
+        public TelaLogin()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void Acesso11_Click(object sender, EventArgs e)
+        {
+            LoginAcesso LoginAlx = new LoginAcesso();
+            LoginAlx.MostrarImgAlx();
+            LoginAlx.Show();
+        }
+
+        private void Acesso12_Click(object sender, EventArgs e) //loja
+        {
+            LoginAcesso LoginLoja = new LoginAcesso();
+            LoginLoja.MostrarImgLoja();
+            LoginLoja.Show();
+        }
+    }
+}
+
